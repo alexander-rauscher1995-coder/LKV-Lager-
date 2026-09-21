cd34595241f3ee9ea85a4ab25cb6356c3b1532c4
alexander-rauscher1995-coder <alexander-rauscher1995@hotmail.com>
2026-09-21 09:45:58 +0200

    V148 cache cloud runtime assets

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-const CACHE='fitness-coach-v4';
-const CORE=['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE='fitness-coach-v5';
+const CORE=[
+ './','./index.html','./manifest.json','./icon.svg',
+ './cloud/sync-adapter.js','./cloud/snapshot.js'
+];
 
 self.addEventListener('install',event=>{
- event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+ event.waitUntil(
+  caches.open(CACHE)
+   .then(c=>c.addAll(CORE))
+   .then(()=>self.skipWaiting())
+ );
 });
 
 self.addEventListener('activate',event=>{
  event.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  caches.keys()
+   .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
    .then(()=>self.clients.claim())
  );
 });
