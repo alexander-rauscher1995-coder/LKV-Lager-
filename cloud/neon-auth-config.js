267ca17b0aeca5774e56c9808166035aeaec119d
alexander-rauscher1995-coder <alexander-rauscher1995@hotmail.com>
2026-09-21 11:26:01 +0200

    Add Neon Auth public configuration

---
+++ cloud/neon-auth-config.js
@@ -0,0 +1,5 @@
+/* Public Neon Auth configuration. No secrets belong here. */
+window.FITNESS_AUTH_CONFIG={
+  enabled:true,
+  authUrl:'https://ep-weathered-sunset-b2a14fxa.neonauth.c-6.eu-central-1.aws.neon.tech/neondb/auth'
+};
