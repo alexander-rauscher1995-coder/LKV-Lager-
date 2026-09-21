da1e73a3ec5e134b103247847bc10e4c22179f87
alexander-rauscher1995-coder <alexander-rauscher1995@hotmail.com>
2026-09-21 16:11:46 +0200

    Refresh calculator fix version

--- cloud/runtime-config.js
+++ cloud/runtime-config.js
@@ -5,7 +5,7 @@ window.FITNESS_CLOUD_CONFIG={
 };
 
 (function(){
-  ['calculator-pro.js?v=162','training-pro.js?v=1','nutrition-pro.js?v=1','progress-pro.js?v=1'].forEach(function(src){
+  ['calculator-pro.js?v=163','training-pro.js?v=1','nutrition-pro.js?v=1','progress-pro.js?v=1'].forEach(function(src){
     var s=document.createElement('script');
     s.src='./'+src;
     s.defer=true;
