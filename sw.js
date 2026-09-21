4701249082c2a41adeaf3eed627b6cd5674ab92c
alexander-rauscher1995-coder <alexander-rauscher1995@hotmail.com>
2026-09-21 09:16:12 +0200

    improve PWA cache lifecycle

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE='fitness-coach-v2';
+const CACHE='fitness-coach-v3';
 const CORE=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+ event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+   .then(()=>self.clients.claim())
+ );
+});
+
 self.addEventListener('fetch',event=>{
  if(event.request.method!=='GET')return;
- event.respondWith(fetch(event.request).then(response=>{
-   const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;
- }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+ event.respondWith(
+  fetch(event.request).then(response=>{
+   if(response&&response.ok){
+    const copy=response.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy));
+   }
+   return response;
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+ );
 });
